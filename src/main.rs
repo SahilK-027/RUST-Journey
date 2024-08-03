a446807ce7c337eeb4db7a425d9491ea223b0eb2
SahilK-027 <sahilkandhare07@gmail.com>
2024-08-03 11:36:19 +0530

    💄 formatting

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,39 @@
 // Basic variable declaration
 /**
- * You can define variables using the `let` keyword
- * You can assign the type of variable, or it can be `inferred` as well.
+ * You can define variables using the `let` keyword.
+ * You can assign the type of variable explicitly, or it can be inferred by the compiler.
  */
 
+/// Demonstrates working with numbers in Rust.
 fn type_number() {
-    let num: i32 = 10; // We have assigned type to as signed integer of 32 bit
+    // Explicit type annotation
+    let num: i32 = 10; // Signed 32-bit integer
+    // Inferred type
     let x = 42; // The type of `x` is inferred to be `i32` (an integer)
+    // Unsigned 32-bit integer
     let y: u32 = 20;
+    // 32-bit floating-point number
     let z: f32 = 20.1;
 
+    // Uncommenting the following line will cause an overflow error
     // let a: i8 = 1024; // Overflow
 
-    // In Rust, the print! macro uses formatting specifiers to determine how to print the provided arguments.
-    // The correct way to use print! is to include a formatting specifier within the string, such as {} for a placeholder.
-    print!("x: {}, y: {}, z: {}", x, y, z);
+    // Print variables using formatting specifiers
+    println!("x: {}, y: {}, z: {}", x, y, z);
 }
 
+/// Demonstrates working with boolean values in Rust.
 fn type_boolean() {
+    // Immutable boolean variable
     let is_raining = true;
+    // Mutable boolean variable
     let mut is_sunny = false;
 
-    // is_raining = false; // You cannot do this all variables in rust are immutable by default
-    is_sunny = true; // you can do this because is_sunny is mutable because of `mut` keyword
+    // Uncommenting the following line will cause a compile-time error
+    // is_raining = false; // All variables in Rust are immutable by default
+
+    // Changing the value of a mutable variable
+    is_sunny = true;
 
     // Use booleans in an if statement
     if is_raining {
@@ -34,6 +45,7 @@ fn type_boolean() {
     }
 }
 
+/// Demonstrates working with strings in Rust.
 fn type_string() {
     // Mutability
     // Immutable &str: String slices (&str) are immutable, meaning you cannot change the content they point to.
@@ -45,7 +57,7 @@ fn type_string() {
     // Theory
     // Strings are considered more complex in Rust (and in general) for several reasons:
     // Rust's ownership system is designed to ensure memory safety without a garbage collector.
-    // Strings, especially String type, are dynamically allocated on the heap.
+    // Strings, especially the String type, are dynamically allocated on the heap.
     // Managing the ownership and lifetime of these strings can be challenging, particularly when passing them around in a program.
 
     // Immutable &str
@@ -58,6 +70,8 @@ fn type_string() {
     println!("Mutable name: {}", name_mutable);
 
     let str = String::from("String");
+
+    // Getting the first character
     let char_0 = str.chars().nth(0);
     // Notice `nth` returns an Option<char> because the element at the specified index might not exist.
     // If the index is out of bounds, None is returned; otherwise, it returns Some(char).
@@ -68,6 +82,7 @@ fn type_string() {
 }
 
 fn main() {
+    // Uncomment the function calls below to test each function
     // type_number();
     // type_boolean();
     type_string();
